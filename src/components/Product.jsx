3231d896cce25181bd6202f700904f98d71bdb32
Alex Trzeciak <trebek08@gmail.com>
2017-03-23 09:02:07 -0700

    added product and products components

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,1 +1,13 @@
-Product.jsx
+import React, { Component } from 'react';
+
+export default class Product extends Component {
+
+  	render() {
+  		const product = this.props.product;
+    return (
+    	<div>	
+	        {product.title}
+	    </div>
+    );
+  }
+}
