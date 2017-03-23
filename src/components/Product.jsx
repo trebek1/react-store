25fcf73f3ab42e226dcd4c30708edefe9c0d708c
Alex Trzeciak <trebek08@gmail.com>
2017-03-23 09:13:15 -0700

    display images

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -4,9 +4,26 @@ export default class Product extends Component {
 
   	render() {
   		const product = this.props.product;
+  		const styles = {
+  			'display': 'inline-block',
+  			"height": '300px',
+  			"width" : '300px'
+  			
+  		}
+  		const imageProps = {
+  			"height": "200px",
+  			"width": "200px"
+  		}
+  		const inlineBlock = {
+  			"display": "inline-block"
+  		}
     return (
-    	<div>	
-	        {product.title}
+    	<div style={inlineBlock} >
+    		<div style={styles}>
+    			<img style={imageProps} src={product.img} />
+    			<br/>
+    			{product.title}
+    		</div>
 	    </div>
     );
   }
